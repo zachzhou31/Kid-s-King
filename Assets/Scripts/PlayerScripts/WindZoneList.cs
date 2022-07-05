96619a31fce681e7b9c016597e634bbafadf3268
tinohu <tino_hu@outlook.com>
2022-07-05 12:24:29 +0800

    Class 20220705

--- Assets/Scripts/PlayerScripts/WindZoneList.cs
+++ Assets/Scripts/PlayerScripts/WindZoneList.cs
@@ -10,19 +10,44 @@ public class WindZoneList : MonoBehaviour
     public static WindZoneList Instance;
     
     public List<WindZone> WindZoneLists = new List<WindZone>();
-    void Start()
+
+    Rigidbody m_Rigidbody;
+    [SerializeField] Vector3 m_WindForce;
+
+    private void Awake()
     {
         if (!Instance) Instance = this;
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 _extraSpeed = Vector3.zero;
+        m_WindForce = Vector3.zero;
+
+        //Vector3 _extraSpeed = Vector3.zero;
+        /*
         foreach (WindZone windzone in WindZoneLists)
             _extraSpeed = new Vector3(_extraSpeed.x + windzone.WindSpeed.x, _extraSpeed.y + windzone.WindSpeed.y, _extraSpeed.z + windzone.WindSpeed.z);
         PlayerController.Instance.WindSpeed = _extraSpeed;
+        */
+
+        foreach (var windZone in WindZoneLists)
+        {
+            m_WindForce += windZone.Velocity;
+        }
     }
 
-
+    private void FixedUpdate()
+    {
+        if (WindZoneLists.Count > 0)
+        {
+            m_Rigidbody.AddForce(m_WindForce);
+        }
+    }
 }
--- Assets/Scripts/WindZone.cs
+++ Assets/Scripts/WindZone.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class WindZone : MonoBehaviour
 {
-    public Vector3 WindSpeed = new Vector3(0.1f, 0, 0.1f);
+    public float WindForce = 0.1f;
+
+
+    public Vector3 Velocity
+    {
+        get
+        {
+            return transform.forward * WindForce;
+        }
+    }
+
+
+    //public Vector3 WindSpeed = new Vector3(0.1f, 0, 0.1f);
     // Start is called before the first frame update
  
 
@@ -13,21 +25,31 @@ public class WindZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-       if(! WindZoneList.Instance.WindZoneLists.Contains(this.GetComponent<WindZone>()))
-            WindZoneList.Instance.WindZoneLists.Add(this.GetComponent<WindZone>());
+       //if(! WindZoneList.Instance.WindZoneLists.Contains(this.GetComponent<WindZone>()))
+       //     WindZoneList.Instance.WindZoneLists.Add(this.GetComponent<WindZone>());
+
+        if (!WindZoneList.Instance.WindZoneLists.Contains(this))
+        {
+            WindZoneList.Instance.WindZoneLists.Add(this);
+        }
 
 
     }
 
     private void OnTriggerStay(Collider other)
     {
-        PlayerController.Instance._rigidbody.AddForce(PlayerController.Instance.WindSpeed, ForceMode.VelocityChange);
+        //PlayerController.Instance._rigidbody.AddForce(PlayerController.Instance.WindSpeed, ForceMode.Force);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        WindZoneList.Instance.WindZoneLists.Remove(this.GetComponent<WindZone>());
+        //WindZoneList.Instance.WindZoneLists.Remove(this.GetComponent<WindZone>());
 
-        PlayerController.Instance._rigidbody.velocity -= PlayerController.Instance.WindSpeed;
+        //PlayerController.Instance._rigidbody.velocity -= PlayerController.Instance.WindSpeed;
+
+        if (WindZoneList.Instance.WindZoneLists.Contains(this))
+        {
+            WindZoneList.Instance.WindZoneLists.Remove(this);
+        }
     }
 }
